bbafd6738c7f84cafaac17bab799d0b1961157d1
rans0000 <rans0000@gmail.com>
2022-05-09 21:26:42 +0530

    fix styles across the pages

--- src/scripts/hero-banner.js
+++ src/scripts/hero-banner.js
@@ -1,12 +1,12 @@
-import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
+import Swiper, { Navigation, Pagination, EffectFade, Autoplay } from 'swiper';
 import 'swiper/swiper.scss';
 import 'swiper/modules/pagination/pagination.min.css';
 import 'swiper/modules/navigation/navigation.min.css';
 import 'swiper/modules/effect-fade/effect-fade.min.css';
 
 const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination, EffectFade],
-    autoplay: true,
+    modules: [Navigation, Pagination, EffectFade, Autoplay],
+    autoplay: { delay: 5000, },
     loop: true,
     effect: 'fade',
     fadeEffect: {
